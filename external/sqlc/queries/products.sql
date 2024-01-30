1f2c3a1b19cac27122119e081e94e0874c30b170
IsaacDSC <isaac8.silva@hotmail.com>
2024-01-29 21:33:06 -0300

    feat: create queries and procedure of product

--- external/sqlc/queries/products.sql
+++ external/sqlc/queries/products.sql
@@ -1,14 +1,8 @@
 -- name: GetProducts :many
-SELECT * FROM products;
+select products.*, categories.name as category_name from products join categories on products.category_id = categories.id limit $1;
 
 -- name: CreateProduct :exec
-INSERT INTO products
-"id"
-"code"
-"name"
-"image_url"
-"price"
-"description"
-"active"
-"category_id"
- VALUES($1, $2, $3, $4, $5, $6, $7, $8);
+INSERT INTO products ("id","code","name","image_url","price","description","active","category_id") VALUES($1, $2, $3, $4, $5, $6, $7, $8);
+
+-- name: GetProductByID :one
+SELECT products.*, categories.name as category_name FROM products join categories on products.category_id = categories.id WHERE products.id = $1;

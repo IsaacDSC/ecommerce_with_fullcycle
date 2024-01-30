7f0213f37abda91b96e4317934b3d80588b7ec30
IsaacDSC <isaac8.silva@hotmail.com>
2024-01-29 21:31:38 -0300

    feat: create schema database

--- external/sqlc/schema.sql
+++ external/sqlc/schema.sql
@@ -1,18 +1,3 @@
-CREATE TABLE IF NOT EXISTS "products" (
-  "id" VARCHAR(255) PRIMARY KEY NOT NULL,
-  "code" VARCHAR(255),
-  "name" VARCHAR(255) NOT NULL,
-  "image_url" VARCHAR(255),
-  "price" VARCHAR(255) NOT NULL,
-  "description" VARCHAR(255),
-  "active" BOOLEAN DEFAULT true,
-  "category_id" VARCHAR(255),
-  "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-  "updated_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-  "deleted_at" TIMESTAMPTZ DEFAULT NULL
-);
-
-
 CREATE TABLE IF NOT EXISTS "categories" (
   "id" VARCHAR(255) PRIMARY KEY NOT NULL,
   "name" VARCHAR(255) NOT NULL,
@@ -20,4 +5,19 @@ CREATE TABLE IF NOT EXISTS "categories" (
   "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   "updated_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   "deleted_at" TIMESTAMPTZ DEFAULT NULL
+);
+
+CREATE TABLE IF NOT EXISTS "products" (
+  "id" VARCHAR(255) PRIMARY KEY NOT NULL,
+  "code" VARCHAR(255),
+  "name" VARCHAR(255) NOT NULL,
+  "image_url" VARCHAR(255),
+  "price" INTEGER NOT NULL,
+  "description" VARCHAR(255),
+  "active" BOOLEAN DEFAULT true,
+  "category_id" VARCHAR(255),
+  "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  "updated_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  "deleted_at" TIMESTAMPTZ DEFAULT NULL,
+  FOREIGN KEY (category_id) REFERENCES categories(id)
 );
